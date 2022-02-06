946ffb93bd2a126457b578ba046903df0bdd1b4c
Matthew William Reidy <48036844+Matthew-Reidy@users.noreply.github.com>
2022-02-06 12:25:16 -0800

    Create main.java

---
+++ main.java
@@ -0,0 +1,9 @@
+public class straccess {
+
+    public static void main(String[] args){
+
+        mathLibImplementation Math = new mathLibImplementation();
+        System.out.println(Math.min(5, 5));
+        System.out.println(Math.max(1 , 1));
+
+    }
