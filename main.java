a1c6522bf9cfbc4d628691f2630db6271e8521d5
Matthew William Reidy <48036844+Matthew-Reidy@users.noreply.github.com>
2022-02-18 14:09:50 -0800

    Update main.java

--- main.java
+++ main.java
@@ -1,9 +1,12 @@
 public class straccess {
 
     public static void main(String[] args){
-
+       
         mathLibImplementation Math = new mathLibImplementation();
-        System.out.println(Math.min(5, 5));
-        System.out.println(Math.max(1 , 1));
+        System.out.println(Math.min(2, 5));
+        System.out.println(Math.max(1 , 3));
+        System.out.println(Math.sin(45));
+        System.out.println(Math.toDegrees(2.0));
+        System.out.println(Math.toRadians(1.0));
 
     }
